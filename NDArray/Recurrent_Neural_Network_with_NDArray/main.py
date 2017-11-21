d6afffe120df2b9bd0d86ccb1b184075dd380381
JONGGON <medical18@naver.com>
2017-11-21 17:40:13 +0900

    upload

--- NDArray/Recurrent_Neural_Network_with_NDArray/main.py
+++ NDArray/Recurrent_Neural_Network_with_NDArray/main.py
@@ -1,17 +1,15 @@
 import mxnet as mx
-#implementation
 cell_type = "RNN"
+
 #dataset = MNIST or CIFAR10 or FashionMNIST
 if cell_type=="RNN":
     import RNN
-    RNN.RNN(epoch=1, batch_size=256 , save_period=100 , load_period=100 , learning_rate=0.001, ctx=mx.gpu(0))
-'''
+    RNN.RNN(epoch=10, batch_size=256 , save_period=10 , load_period=10 , learning_rate=0.001, ctx=mx.gpu(0))
 elif cell_type=="LSTM":
     import LSTM
     LSTM.LSTM(epoch=10, batch_size=256 , save_period=10 , load_period=10 , learning_rate=0.001, ctx=mx.gpu(0))
 elif cell_type=="GRU":
-    import RNN
+    import GRU
     GRU.GRU(epoch=10, batch_size=256 , save_period=10 , load_period=10 , learning_rate=0.001, ctx=mx.gpu(0))
-'''
-'''else :
-    print("please write the cell type exactly")'''
+else :
+    print("please write the cell type exactly")
